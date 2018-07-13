b797663a7510045212bea80df97f95e81d70b005
Parker Temple <parkertemple@OverOpsPTs-MacBook-Pro.local>
2018-07-13 13:41:44 -0700

    using new env names to set app and deployment in agent

--- agent/run.sh
+++ agent/run.sh
@@ -1,1 +1,1 @@
-docker run -e "TAKIPI_COLLECTOR_HOST=127.0.0.1" -e "TAKIPI_COLLECTOR_PORT=6060" --net="host" -d agent
+docker run -e "TAKIPI_COLLECTOR_HOST=127.0.0.1" -e "TAKIPI_COLLECTOR_PORT=6060" -e "TAKIPI_NAME=testname" -e "TAKIPI_DEPLOYMENT_NAME=deploy1" --net="host" -d agent
